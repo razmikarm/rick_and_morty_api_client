4480071124d4ee63f8ab5b095df8ccbc32ecbb48
Razmik Poghosyan <razmikarm@gmail.com>
2025-01-31 23:16:35 +0400

    Fix: Moved multiselect functionalty from pages to ids

--- ramapi/resource.py
+++ ramapi/resource.py
@@ -16,21 +16,21 @@ class Resource:
         page_count = self.get()["info"]["pages"]
         results = []
         for page in range(1, page_count + 1):
-            page_data = self.get_pages(page)
+            page_data = self.get_page(page)
             results.extend(page_data["results"])
         return results
 
-    def get_pages(self, *pages: list) -> dict:
-        if len(pages) == 0:
-            raise ValueError("At least one page number must be given")
-        elif len(pages) == 1:
-            page = pages[0]
-        else:
-            page = ','.join([str(p) for p in pages])
+    def get_page(self, page: int) -> dict:
         target_url = f'{self.url}?page={page}'
         return requests.get(target_url).json()
 
-    def get_by_id(self, id: int) -> dict:
+    def get_by_ids(self, *ids: list) -> dict:
+        if len(ids) == 0:
+            raise ValueError("At least one id must be given")
+        elif len(ids) == 1:
+            id = ids[0]
+        else:
+            id = ','.join([str(i) for i in ids])
         target_url = f"{self.url}/{id}"
         return requests.get(target_url).json()
 
